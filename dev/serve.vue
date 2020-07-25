13e3be3c8ea8700a90e980abb7381c4abad2907e
Chris Doty <dotyc@civicinc.com>
2020-07-25 09:49:54 -0500

    fix: first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { CodigoFormSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    CodigoFormSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <codigo-form-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as CodigoFormSample } from './codigo-form-sample.vue';
