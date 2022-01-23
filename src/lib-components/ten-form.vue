be46e600ebb81f66deb19d72ed2905dbe48091ce
Chris Doty <cdotyone@gmail.com>
2022-01-23 14:04:37 -0600

    fix: remove unused data fields

--- src/lib-components/ten-form.vue
+++ src/lib-components/ten-form.vue
@@ -14,14 +14,10 @@
 
 <script>
 import TenWrapper from "./ten-wrapper.vue";
-import TenInput from "./ten-input.vue";
-import TenSelect from "./ten-select.vue";
-import TenToggle from "./ten-toggle.vue";
-import TenTextarea from "./ten-textarea.vue";
 import TenField from "./ten-field.vue";
 export default {
   name: 'ten-form',
-  components: { TenField, TenTextarea, TenToggle, TenSelect, TenInput},
+  components: { TenField },
   extends: TenWrapper,
   props: {
     labels: {
@@ -35,21 +31,6 @@ export default {
     fields: {
       type: Array
     }
-  },
-  data() {
-    return {
-      options: [
-        {name:'Option 1',code:'1'},
-        {name:'Option 2',code:'2'},
-        {name:'Option 3',code:'3'},
-        {name:'Option 4',code:'4'},
-        {name:'Option 5',code:'5'}
-      ],
-      content1:"",
-      contentTextArea:"",
-      s1Value:'1',
-      checkValue:'Y'
-    };
   },
   computed: {
     classes() {
