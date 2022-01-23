adf64f15e247855beb98101dc323b11a5b2c7b0e
Chris Doty <cdotyone@gmail.com>
2022-01-23 13:46:36 -0600

    fix: bad reference

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -3,7 +3,7 @@ export { default as TenWrapper } from './ten-wrapper.vue';
 export { default as TenForm } from './ten-form.vue';
 export { default as TenField } from './ten-field.vue';
 export { default as TenInput } from './ten-input.vue';
-export { default as TenLayout } from './ten-layout';
+export { default as TenLayout } from './ten-layout.vue';
 export { default as TenSelect } from './ten-select.vue';
 export { default as TenToggle } from './ten-toggle.vue';
 export { default as TenTextArea } from './ten-textarea.vue';
