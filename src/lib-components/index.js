803fe2746c41ab68786968c8c3a2e782515f8a82
Chris Doty <cdotyone@gmail.com>
2022-01-23 13:40:48 -0600

    fix: did not register all components

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 export { default as TenWrapper } from './ten-wrapper.vue';
 export { default as TenForm } from './ten-form.vue';
+export { default as TenField } from './ten-field.vue';
 export { default as TenInput } from './ten-input.vue';
+export { default as TenLayout } from './ten-layout';
 export { default as TenSelect } from './ten-select.vue';
 export { default as TenToggle } from './ten-toggle.vue';
 export { default as TenTextArea } from './ten-textarea.vue';
