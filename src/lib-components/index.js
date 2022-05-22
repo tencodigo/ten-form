05b9f90d41aaab17904f533cc0bf002fc5ec46cd
Chris Doty <cdotyone@gmail.com>
2022-05-22 16:24:03 -0500

    fix: lower camel case component export names

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-export { default as TenWrapper } from './ten-wrapper.vue';
-export { default as TenForm } from './ten-form.vue';
-export { default as TenField } from './ten-field.vue';
-export { default as TenInput } from './ten-input.vue';
-export { default as TenLayout } from './ten-layout.vue';
-export { default as TenSelect } from './ten-select.vue';
-export { default as TenToggle } from './ten-toggle.vue';
-export { default as TenTextArea } from './ten-textarea.vue';
+export { default as tenWrapper } from './ten-wrapper.vue';
+export { default as tenForm } from './ten-form.vue';
+export { default as tenField } from './ten-field.vue';
+export { default as tenInput } from './ten-input.vue';
+export { default as tenLayout } from './ten-layout.vue';
+export { default as tenSelect } from './ten-select.vue';
+export { default as tenToggle } from './ten-toggle.vue';
+export { default as tenTextArea } from './ten-textarea.vue';
 
